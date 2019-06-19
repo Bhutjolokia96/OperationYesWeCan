6b8aee71090fd327b5ff93b1e309d4c224d87bcb
rundlettjd <bhutjolokia96@gmail.com>
2019-06-19 01:47:51 -0500

    Creation Day

---
+++ OperationYesWeCan/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OperationYesWeCan
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
