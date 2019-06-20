ddaa78be6ac1b7c84b05ad568ab8d01e4fcf6357
Joseph Rundlett <bhutjolokia96@gmail.com>
2019-06-20 01:20:03 -0500

    Added Barcode Generation and started card swiper functionality

--- OperationYesWeCan/Controllers/HomeController.cs
+++ OperationYesWeCan/Controllers/HomeController.cs
@@ -3,19 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OperationYesWeCan.Models;
+using IronBarCode;
+using Hid.Net;
+using HidLibrary;
+using HidSharp;
+
 
 namespace OperationYesWeCan.Controllers
 {
     public class HomeController : Controller
     {
+
+        private OperationYesWeCanEntities db = new OperationYesWeCanEntities();
+        public BarcodeViewModel barcodeView = new BarcodeViewModel();
+
         public ActionResult Index()
         {
+
+            //List<Barcode> barcodes = db.Barcodes.ToList();
+
+            //HidLibrary.HidDevices.Enumerate();
+
+            //return View(barcodes);
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult generateBarcode()
+        {
+            Barcode barcode = new Barcode();
+            barcode.Date = DateTime.Now;
+
+            db.Barcodes.Add(barcode);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
---
+++ OperationYesWeCan/Models/BarcodeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OperationYesWeCan.Models
+{
+    public class BarcodeViewModel
+    {
+        public List<Barcode> barcodes { get; set; }
+    }
+}
